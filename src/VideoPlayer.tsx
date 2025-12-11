352ce37fca7191aac265f64663a7a00ea52e70a5
Milad123123 <144379761+Milad123123@users.noreply.github.com>
2025-12-11 14:45:02 +0330

    Create VideoPlayer.tsx

---
+++ src/VideoPlayer.tsx
@@ -0,0 +1,24 @@
+import React, { useRef, useEffect } from "react";
+import Hls from "hls.js";
+
+interface VideoPlayerProps {
+  src: string;
+}
+
+export default function VideoPlayer({ src }: VideoPlayerProps) {
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  useEffect(() => {
+    if (!videoRef.current) return;
+
+    if (Hls.isSupported()) {
+      const hls = new Hls();
+      hls.loadSource(src);
+      hls.attachMedia(videoRef.current);
+    } else if (videoRef.current.canPlayType("application/vnd.apple.mpegurl")) {
+      videoRef.current.src = src;
+    }
+  }, [src]);
+
+  return <video ref={videoRef} controls style={{ width: "100%", maxWidth: 800 }} />;
+}
